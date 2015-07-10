302a77350a488f4397797d97a1c25418b7193d01
Theodore Gonzalez <theodore.g@imena.com>
2015-07-10 16:30:08 +0400

    added MenuController, added transition segue

---
+++ Raytejada/Raytejada/Controller/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  Raytejada
+//
+//  Created by ADMIN on 7/10/15.
+//  Copyright (c) 2015 raystudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
